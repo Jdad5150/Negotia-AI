247847a3e8fd234e8ec37d057e30c74e7e065b53
Jesse Little <jjvlittle@gmail.com>
2025-02-12 01:01:10 -0500

    Finished Cleaning Data, updated the readme to explain why parquet for clean data

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # NegotiaAI
+
 Salary Prediction Web Application
+
+The raw data was very messy and large something like 458mb so all .csv files are ignored via .gitignore.
+Cleaned data is saved as a parquet file.
--- model_building/source/main.py
+++ model_building/source/main.py
@@ -1,5 +1,5 @@
 """
-Main script for creating and training a machine learning model using Keras, encoding categorical features, 
+Main script for creating and training a machine learning model using Keras, encoding categorical features,
 and saving the model for inference with TensorFlow.js.
 
 The script performs the following tasks:
@@ -34,12 +34,13 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 import tensorflow as tf
-from tensorflow.keras.models import Sequential # type: ignore
+from tensorflow.keras.models import Sequential  # type: ignore
 from tensorflow.keras.layers import Dense, LeakyReLU, BatchNormalization, Dropout
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import EarlyStopping
 
 seed = 42
+
 
 def create_date():
     """
@@ -47,41 +48,47 @@ def create_date():
     """
     # Define the job titles
     job_titles = [
-    "Software Engineer",
-    "Data Scientist",
-    "Project Manager",
-    "Machine Learning Engineer",
-    "UI/UX Designer",
-    "Product Manager",
-    "DevOps Engineer",
-    "System Administrator",
-    "Cloud Architect",
-    "Database Administrator"
+        "Software Engineer",
+        "Data Scientist",
+        "Project Manager",
+        "Machine Learning Engineer",
+        "UI/UX Designer",
+        "Product Manager",
+        "DevOps Engineer",
+        "System Administrator",
+        "Cloud Architect",
+        "Database Administrator",
     ]
 
     # Create the data
-    data = pd.DataFrame({
-        'title' : np.random.choice(job_titles, 1000),
-        'state' : np.random.choice(['CA', 'NY', 'TX', 'WA', 'FL'], 1000),
-        'salary' : np.random.randint(50000, 200000, 1000),
-        'experience' : np.random.randint(0, 6, 1000),
-        'work_type' : np.random.choice(['Full-time', 'Part-time', 'Contract', 'Remote'], 1000)
-    })
+    data = pd.DataFrame(
+        {
+            "title": np.random.choice(job_titles, 1000),
+            "state": np.random.choice(["CA", "NY", "TX", "WA", "FL"], 1000),
+            "salary": np.random.randint(50000, 200000, 1000),
+            "experience": np.random.randint(0, 6, 1000),
+            "work_type": np.random.choice(
+                ["Full-time", "Part-time", "Contract", "Remote"], 1000
+            ),
+        }
+    )
 
     return data
 
 
-def encode_and_save(df, column_name, save_dir='../../shared', file_name='encoding.json'):
+def encode_and_save(
+    df, column_name, save_dir="../../shared", file_name="encoding.json"
+):
     """
     Encodes the specified column in the DataFrame using LabelEncoder,
     then saves the encoding map to a JSON file.
-    
+
     Parameters:
     - df: pandas DataFrame containing the data.
     - column_name: the name of the column to encode.
     - save_dir: directory to save the encoding map JSON file (default is '../../shared').
     - file_name: name of the JSON file to save the encoding map (default is 'encoding.json').
-    
+
     Returns:
     - df: DataFrame with the encoded column.
     """
@@ -104,17 +111,17 @@ def encode_and_save(df, column_name, save_dir='../../shared', file_name='encodin
     file_path = os.path.join(save_dir, file_name)
 
     # Write the encoding map to a JSON file
-    with open(file_path, 'w') as f:
+    with open(file_path, "w") as f:
         json.dump(encoding_map, f)
 
     # Optionally, print to confirm the file is saved
-    print(f'Encoding map saved to: {file_path}')
-    
+    print(f"Encoding map saved to: {file_path}")
+
     # Return the DataFrame with the encoded column
     return df
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Load the data
     df = create_date()
     print(df.head(5))
@@ -123,21 +130,19 @@ if __name__ == '__main__':
     df = df.dropna()
 
     # Encode the categorical columns
-    df = encode_and_save(df, 'title', file_name='title_encoding.json')
-    df = encode_and_save(df, 'state', file_name='state_encoding.json')
-    df = encode_and_save(df, 'experience', file_name='exp_level_encoding.json')
-    df = encode_and_save(df, 'work_type', file_name='work_type_encoding.json')
-
-
+    df = encode_and_save(df, "title", file_name="title_encoding.json")
+    df = encode_and_save(df, "state", file_name="state_encoding.json")
+    df = encode_and_save(df, "experience", file_name="exp_level_encoding.json")
+    df = encode_and_save(df, "work_type", file_name="work_type_encoding.json")
 
     # Split the data
-    X = df.drop('salary', axis=1)
-    y = df['salary']
+    X = df.drop("salary", axis=1)
+    y = df["salary"]
 
     scaler = StandardScaler()
     X = scaler.fit_transform(X)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y,  random_state=seed)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=seed)
 
     # Create the model
     model = Sequential()
@@ -161,18 +166,26 @@ if __name__ == '__main__':
     # Output layer (regression)
     model.add(Dense(1))
 
-    model.compile(optimizer=Adam(learning_rate=0.001), loss='mean_squared_error')
-
+    model.compile(optimizer=Adam(learning_rate=0.001), loss="mean_squared_error")
 
     # Train the model
-    history = model.fit(X_train, y_train, epochs=100, batch_size=32, validation_data=(X_test, y_test), verbose=0)
+    history = model.fit(
+        X_train,
+        y_train,
+        epochs=100,
+        batch_size=32,
+        validation_data=(X_test, y_test),
+        verbose=0,
+    )
 
-    early_stop = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
+    early_stop = EarlyStopping(
+        monitor="val_loss", patience=5, restore_best_weights=True
+    )
 
     model.summary()
 
     ## Save the model
-    model.save('../../shared/demo_model.keras')
+    model.save("../../shared/demo_model.keras")
 
     # import tensorflowjs as tfjs
     # tfjs.converters.save_keras_model(model, '../../shared/demo_model')
